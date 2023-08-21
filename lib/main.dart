df102a4eeb4a998d3d35d040e4756217f08d86f7
Fabio Oliveira <fabiodeoliveirabr@gmail.com>
2023-08-21 11:21:54 -0300

    Aula 18/08/2023

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TelaFormulario(),
+  ));
+}
+
+//Criar um Widget Scaffold
+class TelaFormulario extends StatefulWidget {
+  const TelaFormulario({super.key});
+
+  @override
+  State<TelaFormulario> createState() => _TelaFormularioState();
+}
+
+class _TelaFormularioState extends State<TelaFormulario> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(50),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Formulário de cadastro",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("Nome"),
+                        border: OutlineInputBorder(),
+                        suffixIcon: Icon(Icons.abc),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("Sobrenome"),
+                        border: OutlineInputBorder(),
+                        suffixIcon: Icon(Icons.abc),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
